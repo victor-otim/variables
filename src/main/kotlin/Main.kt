a2471b0456c59b2fa3261cc8082e0328a7f75b54
Victor Otim <sirotim@gmail.com>
2023-07-26 11:52:25 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    /*println("Hello!")
+    print("hmm")
+    print("hmm")*/
+
+    val num1 = 1
+    var num2 = 5
+    var z = num1 + num2
+
+    print("The sum of $num1 and $num2 is $z")
+}
